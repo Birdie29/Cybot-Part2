cafecb1fe2fc5144a5a4e3520523ec0885d29241
Birdie29 <pc@DESKTOP-F0PVSG0>
2026-05-22 20:10:56 -0700

    favtopic method

--- Cybot/MainWindow.xaml.cs
+++ Cybot/MainWindow.xaml.cs
@@ -21,7 +21,8 @@ namespace Cybot
             InitializeComponent();
            // chatBotGreeting();
             Username();
-     
+            //PressClick();
+            FavTopic();
         }
         /*
         private void chatBotGreeting()
@@ -32,7 +33,7 @@ namespace Cybot
         }
         */
 
-        private void Press_Click(object sender, RoutedEventArgs e)
+        private void PressClick(object sender, RoutedEventArgs e)
         {
             string name = username.Text;
             Username();
@@ -55,9 +56,21 @@ namespace Cybot
 
         private void FavTopic() 
         { 
-
+            string topic = favTopic.Text;
+            if (string.IsNullOrWhiteSpace(topic))
+            {
+                favTopic.Foreground = Brushes.Red;
+                favTopic.Text = "I'm sorry i didn't understand that please re-enter your topic";
+            }
+            else 
+            {
+                favTopic.Foreground = Brushes.Blue;
+                favTopic.Text = "Great! I'll be sure to remember that you're interested in " + topic + ". It's a crucial part of staying safe online.";
+            }
 
         }
+
+        
 
     }
 }
