6f9b2b1ece4fd3c5b6ad8ae059a82eebac6695ac
Birdie29 <pc@DESKTOP-F0PVSG0>
2026-06-05 22:18:48 -0700

    question class containing varibales

---
+++ Cybot/Questions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cybot
+{
+    class Questions
+    {
+        public string txtQuestion { get; set; }
+        public string rb1 { get; set; }
+        public string rb2 { get; set; }
+        public string rb3 { get; set;  }
+        public string rb4 { get; set; }
+        public string correct {  get; set; }
+        public string txtFeedback {  get; set; }
+        
+
+    }
+}
